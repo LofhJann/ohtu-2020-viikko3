16e731462413b46020d13653c7f2ed069216ed74
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-11-09 16:00:59 +0200

    Add tests for reset scenarios

--- HelloCucumber/src/test/java/ohtu/Stepdefs.java
+++ HelloCucumber/src/test/java/ohtu/Stepdefs.java
@@ -1,14 +1,16 @@
 
 package ohtu;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+
 import static org.junit.Assert.*;
 
 public class Stepdefs {
     Counter counter;
-    
+
     @Given("Counter is initialized")
     public void counterIsInitialized() {
         counter = new Counter();
@@ -19,6 +21,16 @@ public class Stepdefs {
         counter.increase();
     }
 
+    @And("it is reset")
+    public void itIsReset() {
+        counter.reset();
+    }
+
+    @Then("value should be 0")
+    public void valueShouldBe0() {
+        assertEquals(0, counter.value());
+    }
+
     @Then("the value should be {int}")
     public void theValueShouldBe(Integer val) {
         assertEquals(val.intValue(), counter.value());
@@ -26,7 +38,7 @@ public class Stepdefs {
 
     @When("it is incremented by {int}")
     public void itIsIncrementedBy(Integer val) {
-         counter.increment(val);       
-    }    
+        counter.increment(val);
+    }
 
 }
