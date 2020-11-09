1a3b10350f6343ac3413845035833b83116ed284
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2020-11-09 15:35:46 +0200

    Add fields, getters, setters, update toString

--- nhlreader/src/main/java/ohtu/Player.java
+++ nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,64 @@
 
 package ohtu;
 
+import java.util.StringJoiner;
+
 public class Player {
     private String name;
+    private int goals;
+    private int assists;
+    private int penalties;
+    private String team;
+    private String nationality;
+    private String birthdate;
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -14,7 +70,14 @@ public class Player {
 
     @Override
     public String toString() {
-        return name;
+        return new StringJoiner(", ", Player.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("goals=" + goals)
+                .add("assists=" + assists)
+                .add("penalties=" + penalties)
+                .add("team='" + team + "'")
+                .add("nationality='" + nationality + "'")
+                .add("birthdate='" + birthdate + "'")
+                .toString();
     }
-      
 }
